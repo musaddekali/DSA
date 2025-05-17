bdd5417068bd4f296e4e8640165eb65726accda7
musaddekali <musaddekali.dev@gmail.com>
2025-05-17 15:32:07 +0600

    testing selection sort

--- selectionSort.js
+++ selectionSort.js
@@ -1,8 +1,24 @@
 const numbers = [4, 6, 1, 33, 5, 10];
 
 function selectionSort(arr) {
-    const data = arr.slice();
-    return data;
+  const data = arr.slice();
+  const n = data.length;
+  for( let i = 0; i < n - 1; i++) {
+    let minIndex = i;
+
+    for (let j = i + 1; j < n; j++) {
+        console.log({minIndex, j})
+        if(data[j] < data[minIndex]) {
+            minIndex = j;
+        }
+    }
+    
+    [data[i], data[minIndex]] = [data[minIndex], data[i]]
+    
+    console.log(`====== Main ${i} loop end ======`)
+
+  }
+  return data;
 }
 
-console.log(selectionSort(numbers))
+console.log(selectionSort(numbers));
