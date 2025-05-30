1f121e98ae1c0b42e3c6b63a63dcd333438e5a74
musaddekali <musaddekali.dev@gmail.com>
2025-05-30 22:44:18 +0600

    will start insertionSort

---
+++ insertionSort.js
@@ -0,0 +1,9 @@
+const numbers = [4, 6, 1, 33, 5, 10];
+
+function insertionSort(arr) {
+    const data = arr.slice();
+    const n = arr.length;
+    return data;
+}
+
+console.log(insertionSort(numbers))
--- selectionSort.js
+++ selectionSort.js
@@ -3,23 +3,23 @@ const numbers = [4, 6, 1, 33, 5, 10];
 function selectionSort(arr) {
   const data = arr.slice();
   const n = data.length;
-  for( let i = 0; i < n - 1; i++) {
+  for (let i = 0; i < n - 1; i++) {
     let minIndex = i;
 
     for (let j = i + 1; j < n; j++) {
-        console.log({minIndex, j})
-        if(data[j] < data[minIndex]) {
-            minIndex = j;
-        }
+      if (data[j] < data[minIndex]) {
+        minIndex = j;
+      }
     }
-    
-    // normal way 
+
+    // normal way
     // const minValue = data.splice(minIndex, 1)[0];
     // data.splice(i, 0, minValue);
 
-    // efficient way
-    [data[i], data[minIndex]] = [data[minIndex], data[i]]
-
+    // efficient way 
+    if (minIndex !== i) {
+      [data[i], data[minIndex]] = [data[minIndex], data[i]];
+    }
   }
   return data;
 }
